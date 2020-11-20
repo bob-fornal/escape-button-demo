eef6f204c7e8e0a4aa9c4378d67281bdd09937f4
Bob Fornal <bob.fornal@leadingedje.com>
2020-11-20 10:54:07 -0500

    first commit

---
+++ escape/index.js
@@ -0,0 +1,61 @@
+
+class EscapeHandler {
+  constructor (images = [], btnText = 'ESCAPE', exitText = 'EXIT') {
+    this.images = images;
+    this.btnText = btnText;
+    this.exitText = exitText;
+
+    this.cover = document.createElement('div');
+    this.cover.classList.add('cover-wrapper');
+    
+    const body = document.querySelector('body');
+    body.appendChild(this.cover);
+
+    this.inner = document.createElement('div');
+    this.inner.classList.add('cover-content');
+
+    this.appendAllImages();
+    this.appendExit();
+
+    this.cover.appendChild(this.inner);
+  }
+
+  trigger() {
+    const state = [...this.cover.classList].includes('visible');
+
+    if (state === true) {
+      this.cover.classList.remove('visible');
+      this.cover.scrollTop = 0;
+    } else {
+      this.cover.classList.add('visible');
+      this.cover.scrollTop = 0;
+    }
+  }
+
+  appendAllImages() {
+    this.images.forEach(image => this.appendImage(image));
+  }
+
+  appendImage(image) {
+    const imgWrapper = document.createElement('div');
+
+    const anchor = document.createElement('a');
+    anchor.setAttribute('href', image.location);
+  
+    const img = document.createElement('img');
+    img.setAttribute('src', image.file);
+  
+    anchor.appendChild(img);
+    imgWrapper.appendChild(anchor);
+    
+    this.inner.appendChild(imgWrapper);
+  }
+
+  appendExit() {
+    const exit = document.createElement('div');
+    exit.classList.add('exit-scramble');
+    exit.innerText = this.exitText;
+    exit.onclick = this.trigger.bind(this);
+    this.inner.appendChild(exit);
+  }
+}
---
+++ index.js
@@ -0,0 +1,14 @@
+
+const images = [
+  { file: './images/google-images-001/google-header.webp', location: 'https://www.google.com/search?q=coffee&oq=coffee' },
+  { file: './images/google-images-001/coffee-wikipedia.webp', location: 'https://en.wikipedia.org/wiki/Coffee' },
+  { file: './images/google-images-001/coffee-nca.webp', location: 'https://www.ncausa.org/About-Coffee/What-is-Coffee' },
+  { file: './images/google-images-001/coffee-benefits.webp', location: 'https://www.medicalnewstoday.com/articles/270202' },
+  { file: './images/google-images-001/coffee-amazon.webp', location: 'https://www.amazon.com/coffee/s?k=coffee' },
+  { file: './images/google-images-001/coffee-peets.webp', location: 'https://www.peets.com/coffee' },
+  { file: './images/google-images-001/coffee-stumptown.webp', location: 'https://www.stumptowncoffee.com/' },
+  { file: './images/google-images-001/coffee-footer.webp', location: 'https://www.google.com/search?q=coffee&oq=coffee' }
+];
+
+const escape = new EscapeHandler(images);
+
